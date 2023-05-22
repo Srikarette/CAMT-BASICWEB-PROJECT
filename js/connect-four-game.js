5162cfb37c5fbd569639febd219e2315f1cc0851
Thitisan Kankathok !! <noonthitisan@gmail.com>
2023-05-22 14:47:00 +0700

    add how to play popup

--- js/connect-four-game.js
+++ js/connect-four-game.js
@@ -6,10 +6,13 @@ const playerTurn = document.getElementsByClassName("player-turn")[0];
 const resetButton = document.querySelector(".reset-button");
 const p1Time = document.querySelector(".p1-time");
 const p2Time = document.querySelector(".p2-time");
+var infoBtn = document.querySelector(".information-element");
+var bgBtn = document.querySelector(".background-element");
 let currentPlayer = "red";
 var betal_played = 0;
-var p1timer = 4000;
-var p2timer = 4000;
+var p1timer = 120000; //120000 = 2 Minute
+var p2timer = 120000;
+//STUPID JS USE P2TIMER->Red , P1TIMER-> Yellow
 //p1timer = setTime(p1timer);
 //p2timer = setTime(p2timer);
 
@@ -18,6 +21,27 @@ var myInterval;
 window.onload = function () {
   playerTurn.textContent = `Current Player : ${currentPlayer.toUpperCase()}`;
   setBetalStand();
+  infoBtn.addEventListener("click",()=>{
+    Swal.fire({
+      width : '900px',
+      customClass: {
+        htmlContainer: 'how-to-class'
+      },
+      html:
+      '<h2>HOW TO PLAY</h2><br><br>' +
+      '<ul>'+
+      '<li>First, decide who goes first and what color each player will have.</li>'+
+      '<li>Players must alternate turns, and only one disc can be dropped in each turn.</li>'+
+      '<li>On your turn, drop one of your colored discs from the top into any of the seven slots.</li>'+
+      '<li>The game ends when there is a 4-in-a-row or a stalemate.</li>'+
+      '<li>The starter of the previous game goes second on the next game.</li>'+
+      '</ul>',
+      footer: '<a href="https://www.gamesver.com/the-rules-of-connect-4-according-to-m-bradley-hasbro/">More rule ?</a>'
+    });
+  })
+  bgBtn.addEventListener("click",()=>{
+    Swal.fire('Somethings');
+  })
 };
 
 // Helper function to check for four in a row
@@ -270,6 +294,6 @@ function alertEvent(winplayer, title) {
     }).then(() => {
       resetBoard();
     });
-  }, 1000);
+  }, 1500);
   resetButton.disabled = false;
 }
